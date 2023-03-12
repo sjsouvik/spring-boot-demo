5e18afb62ca72f5e55754be2e672ddffa8e8560c
Souvik Jana <souvikjana2013@gmail.com>
2023-03-12 19:03:59 +0530

    feat: add a default route

---
+++ src/main/java/com/example/springbootdemo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class HomeController {
+
+  @GetMapping
+  public String greeting() {
+    return "Welcome to my 1st Spring boot app.";
+  }
+}
